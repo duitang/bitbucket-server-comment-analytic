d97ecdedb2deec8388913b065cdea2d404be75e7
Tim Pettersen <tim@atlassian.com>
2012-12-06 09:50:58 +1100

    AMPSify, and make ArchiveExitHandler extend DefaultCommandExitHandler

--- src/main/java/com/atlassian/stash/archive/ArchiveExitHandler.java
+++ src/main/java/com/atlassian/stash/archive/ArchiveExitHandler.java
@@ -4,30 +4,26 @@ import com.atlassian.stash.exception.CommandFailedException;
 import com.atlassian.stash.exception.NoSuchEntityException;
 import com.atlassian.stash.i18n.I18nService;
 import com.atlassian.stash.repository.Repository;
-import com.atlassian.stash.scm.CommandExitHandler;
+import com.atlassian.stash.scm.DefaultCommandExitHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class ArchiveExitHandler implements CommandExitHandler {
+public class ArchiveExitHandler extends DefaultCommandExitHandler {
 
     private final I18nService i18nService;
     private final Repository repository;
     private final String ref;
 
     public ArchiveExitHandler(I18nService i18nService, Repository repository, String ref) {
+        super(i18nService);
         this.i18nService = i18nService;
         this.repository = repository;
         this.ref = ref;
     }
 
     @Override
-    public void onCancel(@Nonnull String command, int exitCode, @Nullable String stdErr, @Nullable Throwable thrown) {
-        onExit(command, exitCode, stdErr, thrown);
-    }
-
-    @Override
-    public void onExit(@Nonnull String command, int exitCode, @Nullable String stdErr, @Nullable Throwable thrown) {
+    public void onError(@Nonnull String command, int exitCode, @Nullable String stdErr, @Nullable Throwable thrown) {
         if ("fatal: Not a valid object name".equals(stdErr)) {
             throw new NoSuchEntityException(i18nService.getKeyedText("stash.archive.object.not.found",
                     "{0} does not exist in repository ''{1}''", ref, repository.getName()));
